0ee226cfe411975d13128a6f96185561324756bc
Caleb <kalebotchere@gmail.com>
2025-08-15 16:53:30 +0000

    Initial Commit

---
+++ civic.2/civic_2App.swift
@@ -0,0 +1,17 @@
+//
+//  civic_2App.swift
+//  civic.2
+//
+//  Created by Caleb Otchere on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct civic_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ civic.2Tests/civic_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  civic_2Tests.swift
+//  civic.2Tests
+//
+//  Created by Caleb Otchere on 15/08/2025.
+//
+
+import Testing
+@testable import civic_2
+
+struct civic_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
